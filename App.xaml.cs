65f1aeb07e906b9f3da2da3433e8e8bc50f2fcff
hamano1204 <45487956+hamano1204@users.noreply.github.com>
2026-04-24 00:53:22 +0900

    Edge Revealの対応

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,5 @@
-using System.Configuration;
-using System.Data;
 using System.Windows;
+using System.Threading;
 
 namespace SideBarTaskSwitcher;
 
@@ -9,5 +8,39 @@ namespace SideBarTaskSwitcher;
 /// </summary>
 public partial class App : System.Windows.Application
 {
+    private static Mutex _mutex = null;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        const string appName = "SideBarTaskSwitcher-Unique-ID";
+        bool createdNew;
+
+        _mutex = new Mutex(true, appName, out createdNew);
+
+        if (!createdNew)
+        {
+            // App is already running!
+            _mutex = null;
+            MessageBox.Show("アプリは既に起動しています。", "SideBar Task Switcher", MessageBoxButton.OK, MessageBoxImage.Information);
+            System.Windows.Application.Current.Shutdown();
+            return;
+        }
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (_mutex != null)
+        {
+            try
+            {
+                _mutex.ReleaseMutex();
+            }
+            catch { }
+            _mutex.Close();
+        }
+        base.OnExit(e);
+    }
 }
 
